59450d2fe039e762137f6cfa73f2f6c3451014e2
suraphon <suraphon.bsk@gmail.com>
2025-03-27 15:57:40 +0700

    flex json

--- resources/data/showcase.js
+++ resources/data/showcase.js
@@ -1,5 +1,15 @@
 import Restaurant from '~/resources/json/showcase/restaurant.json'
 import Apparel from '~/resources/json/showcase/apparel.json'
+import Hotel from '~/resources/json/showcase/hotel.json'
+import LocalSearch from '~/resources/json/showcase/local-search.json'
+import RealEstate from '~/resources/json/showcase/real-estate.json'
+import Social from '~/resources/json/showcase/social.json'
+import TodoApp from '~/resources/json/showcase/todo-app.json'
+import Transit from '~/resources/json/showcase/transit.json'
+import Receipt from '~/resources/json/showcase/receipt.json'
+import Shopping from '~/resources/json/showcase/shopping.json'
+import Menu from '~/resources/json/showcase/menu.json'
+import Ticket from '~/resources/json/showcase/ticket.json'
 
 const data = [
   {
@@ -11,6 +21,56 @@ const data = [
     name: 'Apparel',
     preview: 'https://developers-resource.landpress.line.me/fx/snapshot/captcha13.png',
     json: Apparel,
+  },
+  {
+    name: 'Hotel',
+    preview: 'https://developers-resource.landpress.line.me/fx/snapshot/captcha8.png',
+    json: Hotel,
+  },
+  {
+    name: 'Local Search',
+    preview: 'https://developers-resource.landpress.line.me/fx/snapshot/captcha12.png',
+    json: LocalSearch,
+  },
+  {
+    name: 'Real Estate',
+    preview: 'https://developers-resource.landpress.line.me/fx/snapshot/captcha11.png',
+    json: RealEstate,
+  },
+  {
+    name: 'Social',
+    preview: 'https://developers-resource.landpress.line.me/fx/snapshot/captcha10.png',
+    json: Social,
+  },
+  {
+    name: 'TodoApp',
+    preview: 'https://developers-resource.landpress.line.me/fx/snapshot/captcha7.png',
+    json: TodoApp,
+  },
+  {
+    name: 'Transit',
+    preview: 'https://developers-resource.landpress.line.me/fx/snapshot/captcha9.png',
+    json: Transit,
+  },
+  {
+    name: 'Receipt',
+    preview: 'https://developers-resource.landpress.line.me/fx/snapshot/captcha3.png',
+    json: Receipt,
+  },
+  {
+    name: 'Shopping',
+    preview: 'https://developers-resource.landpress.line.me/fx/snapshot/captcha4.png',
+    json: Shopping,
+  },
+  {
+    name: 'Menu',
+    preview: 'https://developers-resource.landpress.line.me/fx/snapshot/captcha5.png',
+    json: Menu,
+  },
+  {
+    name: 'Ticket',
+    preview: 'https://developers-resource.landpress.line.me/fx/snapshot/captcha6.png',
+    json: Ticket,
   },
 ]
 
