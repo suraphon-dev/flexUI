6de2cbd7e6f645e955dde515eef8c7236c9622fa
suraphon <suraphon.bsk@gmail.com>
2025-03-28 09:43:26 +0700

    home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,11 @@
 <template>
-   <div>
-      <h1>Home Page</h1>
-   </div>
+  <figure class="diff aspect-16/9" tabindex="0">
+    <div class="diff-item-1" role="img">
+      <div class="bg-primary text-primary-content grid place-content-center text-9xl font-black">FlexUI</div>
+    </div>
+    <div class="diff-item-2" role="img" tabindex="0">
+      <div class="bg-base-200 grid place-content-center text-9xl font-black">FlexUI</div>
+    </div>
+    <div class="diff-resizer"></div>
+  </figure>
 </template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
