7c6fc9d23d606024a94bf0b03fa690d0f076f341
suraphon <suraphon.bsk@gmail.com>
2025-03-28 09:57:25 +0700

    update flex simple

--- resources/data/index.js
+++ resources/data/index.js
@@ -1,17 +1,12 @@
-import json1 from '~/resources/json/showcase/restaurant.json'
+import simple from '~/resources/json/table/simple.json'
+import simpleImage from '~/public/image/simple.png'
 
 const data = [
   {
-    name: 'Restaurant',
-    preview: 'https://img.daisyui.com/images/stock/453966.webp',
-    json: json1,
+    name: 'Simple',
+    preview: simpleImage,
+    json: simple,
   },
-  {
-    name: 'Restaurant',
-    preview: 'https://img.daisyui.com/images/stock/453966.webp',
-    json: json1,
-  },
-  
 ]
 
 export default data
