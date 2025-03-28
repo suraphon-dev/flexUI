f0131828b8e366db35dd3f4e87d2ee343f088920
suraphon <suraphon.bsk@gmail.com>
2025-03-28 10:18:48 +0700

    upate grid

--- pages/showcase.vue
+++ pages/showcase.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-3 gap-4">
+  <div class="grid grid-cols-1  lg:grid-cols-3 gap-4">
     <div v-for="(item, index) in dataFlex" :key="index">
       <HeadTag :title="item.name" />
       <TabContents typeMode="showcase" :parentName="`${item.name}_${index}`" :preview="item.preview" :json="item.json" />
--- pages/table.vue
+++ pages/table.vue
@@ -1,7 +1,9 @@
 <template>
-  <div v-for="(item, index) in dataFlex" :key="index">
-    <HeadTag :title="item.name" />
-    <TabContents typeMode="mockup-phone" :parentName="`${item.name}_${index}`" :preview="item.preview" :json="item.json" />
+  <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+    <div v-for="(item, index) in dataFlex" :key="index">
+      <HeadTag :title="item.name" />
+      <TabContents typeMode="mockup-phone" :parentName="`${item.name}_${index}`" :preview="item.preview" :json="item.json" />
+    </div>
   </div>
 </template>
 
