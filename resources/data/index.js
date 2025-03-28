4046188013ce19f4d5ef65943d84c5b3239e3968
suraphon <suraphon.bsk@gmail.com>
2025-03-28 10:18:29 +0700

    update flex simple-2

--- resources/data/index.js
+++ resources/data/index.js
@@ -1,11 +1,18 @@
 import simple from '~/resources/json/table/simple.json'
 import simpleImage from '~/public/image/simple.png'
+import simple2 from '~/resources/json/table/simple-2.json'
+import simpleImage2 from '~/public/image/simple-2.png'
 
 const data = [
   {
     name: 'Simple',
     preview: simpleImage,
     json: simple,
+  },
+  {
+    name: 'Simple-2',
+    preview: simpleImage2,
+    json: simple2,
   },
 ]
 
